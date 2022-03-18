e2597c053b2b2d417d03ce43074218d17e378622
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-18 10:23:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record) {
+   const winYear = record.find(game  => game.result === 'W')
+         if(winYear) {
+             return winYear.year
+         }
+         else {return undefined}
+}
+    
+
+
